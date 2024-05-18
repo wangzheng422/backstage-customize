24f2f5044beff75100f9cfff6edf0437e2922e42
Wang Zheng <wangzheng422@gmail.com>
2024-05-18 23:35:05 +0800

    build image

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # backstage-customize
+
+customized using upstream:
+- https://github.com/backstage/backstage/blob/master/plugins/catalog-backend-module-msgraph/README.md
+- https://github.com/backstage/backstage/blob/master/plugins/catalog-backend-module-azure/README.md
+- https://github.com/backstage/community-plugins/blob/main/workspaces/azure-devops/plugins/azure-devops-backend/README.md
+- https://github.com/backstage/community-plugins/blob/main/workspaces/azure-devops/plugins/azure-devops/README.md
+
+```bash
+
+# run the rhdp to get content
+podman run -it --rm --entrypoint /bin/bash registry.redhat.io/rhdh/rhdh-hub-rhel9:1.1
+
+
+# build the backstage image
+mkdir -p /data
+cd /data
+
+git clone https://github.com/wangzheng422/backstage-customize
+cd backstage-customize
+
+
+podman build -t quay.io/wangzheng422/qimgs:rhdh-hub-rhel9-1.1-2024.05.18.v01 -f Dockerfile .
+
+podman push quay.io/wangzheng422/qimgs:rhdh-hub-rhel9-1.1-2024.05.17.v02
+
+
+```
---
+++ index.ts
@@ -0,0 +1,131 @@
+
+import { rootHttpRouterServiceFactory } from '@backstage/backend-app-api';
+import { createBackend } from '@backstage/backend-defaults';
+import { PackageRoles } from '@backstage/cli-node';
+import {
+  dynamicPluginsFeatureDiscoveryServiceFactory,
+  dynamicPluginsServiceFactory,
+  dynamicPluginsSchemasServiceFactory,
+  dynamicPluginsFrontendSchemas,
+  dynamicPluginsRootLoggerServiceFactory,
+} from '@backstage/backend-dynamic-feature-service';
+import {
+  rbacDynamicPluginsProvider,
+  pluginIDProviderService,
+} from './modules/rbacDynamicPluginsModule';
+import { metricsHandler } from './metrics';
+import { statusCheckHandler } from '@backstage/backend-common';
+import { RequestHandler } from 'express';
+import * as path from 'path';
+import { CommonJSModuleLoader } from './loader';
+
+// begin
+// https://github.com/backstage/community-plugins/blob/main/workspaces/azure-devops/plugins/azure-devops-backend/README.md
+import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/alpha';
+import { coreServices, createBackendModule } from '@backstage/backend-plugin-api';
+import { AzureDevOpsAnnotatorProcessor } from '@backstage-community/plugin-azure-devops-backend';
+const catalogModuleCustomExtensions = createBackendModule({
+  pluginId: 'catalog', // name of the plugin that the module is targeting
+  moduleId: 'custom-extensions',
+  register(env) {
+    env.registerInit({
+      deps: {
+        catalog: catalogProcessingExtensionPoint,
+        config: coreServices.rootConfig,
+      },
+      async init({ catalog, config }) {
+        catalog.addProcessor(AzureDevOpsAnnotatorProcessor.fromConfig(config));
+      },
+    });
+  },
+});
+// end
+// https://github.com/backstage/community-plugins/blob/main/workspaces/azure-devops/plugins/azure-devops-backend/README.md
+
+const backend = createBackend();
+
+backend.add(
+  rootHttpRouterServiceFactory({
+    configure(context) {
+      let healthCheckHandler: RequestHandler | undefined;
+
+      const { app, routes, middleware } = context;
+      app.use(middleware.helmet());
+      app.use(middleware.cors());
+      app.use(middleware.compression());
+      app.use(middleware.logging());
+      app.use('/healthcheck', async (_, response, next) => {
+        if (!healthCheckHandler) {
+          healthCheckHandler = await statusCheckHandler();
+        }
+        healthCheckHandler(_, response, next);
+      });
+      app.use('/metrics', metricsHandler());
+      app.use(routes);
+      app.use(middleware.notFound());
+      app.use(middleware.error());
+    },
+  }),
+);
+backend.add(dynamicPluginsFeatureDiscoveryServiceFactory()); // overridden version of the FeatureDiscoveryService which provides features loaded by dynamic plugins
+backend.add(
+  dynamicPluginsServiceFactory({
+    moduleLoader: logger => new CommonJSModuleLoader(logger),
+  }),
+);
+
+backend.add(
+  dynamicPluginsSchemasServiceFactory({
+    schemaLocator(pluginPackage) {
+      const platform = PackageRoles.getRoleInfo(
+        pluginPackage.manifest.backstage.role,
+      ).platform;
+      return path.join(
+        platform === 'node' ? 'dist' : 'dist-scalprum',
+        'configSchema.json',
+      );
+    },
+  }),
+);
+backend.add(dynamicPluginsFrontendSchemas());
+backend.add(dynamicPluginsRootLoggerServiceFactory());
+
+backend.add(import('@backstage/plugin-app-backend/alpha'));
+backend.add(
+  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+);
+
+backend.add(import('@backstage/plugin-catalog-backend/alpha'));
+
+// TODO: Probably we should now provide this as a dynamic plugin
+backend.add(import('@backstage/plugin-catalog-backend-module-openapi'));
+
+backend.add(import('@backstage/plugin-proxy-backend/alpha'));
+
+// TODO: Check in the Scaffolder new backend plugin why the identity is not passed and the default is built instead.
+backend.add(import('@backstage/plugin-scaffolder-backend/alpha'));
+
+backend.add(import('@backstage/plugin-search-backend/alpha'));
+backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
+
+// TODO: We should test it more deeply. The structure is not exactly the same as the old backend implementation
+backend.add(import('@backstage/plugin-events-backend/alpha'));
+
+backend.add(import('@janus-idp/backstage-plugin-rbac-backend'));
+backend.add(pluginIDProviderService);
+backend.add(rbacDynamicPluginsProvider);
+
+backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('./modules/authProvidersModule'));
+
+backend.add(import('@internal/plugin-dynamic-plugins-info-backend'));
+backend.add(import('@internal/plugin-scalprum-backend'));
+
+// begin
+// https://github.com/backstage/community-plugins/blob/main/workspaces/azure-devops/plugins/azure-devops-backend/README.md
+backend.add(import('@backstage-community/plugin-azure-devops-backend'));
+backend.add(catalogModuleCustomExtensions());
+// end
+// https://github.com/backstage/community-plugins/blob/main/workspaces/azure-devops/plugins/azure-devops-backend/README.md
+
+backend.start();
